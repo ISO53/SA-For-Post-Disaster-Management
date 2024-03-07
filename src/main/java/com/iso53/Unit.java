a9bf5b900c09e73402c6c7a8c77ba9cf6550075f
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-03-07 10:33:23 +0300

    Update Unit.java

--- src/main/java/com/iso53/Unit.java
+++ src/main/java/com/iso53/Unit.java
@@ -1,18 +1,55 @@
 package com.iso53;
 
 public class Unit {
-    String name;
-    int type;
 
-    public Unit(int type) {
+    String type;
+    String name;
+    int lastLocationIndex;
+
+    public Unit(String type) {
         this.type = type;
+        this.name = getNameFromType(type);
+        this.lastLocationIndex = 14;
+    }
+
+    private static String getNameFromType(String type) {
+        int pieceLength = type.length() / 3;
+
+        String piece1 = type.substring(0, pieceLength);
+        String piece2 = type.substring(pieceLength, 2 * pieceLength);
+        String piece3 = type.substring(2 * pieceLength);
+
+        int n = 0;
+        if (!piece1.equals("000")) {
+            n = 0;
+        } else if (!piece2.equals("000")) {
+            n = 1;
+        } else if (!piece3.equals("000")) {
+            n = 2;
+        }
+
+        return ProblemData.UNIT_NAMES[n];
     }
 
     public boolean canHandle(int incident) {
-        return ProblemData.UNITS_CAPABILITIES[type][incident] == 1;
+//        return ProblemData.UNITS_CAPABILITIES[type][incident] != -1;
+        // ??????
+        return false;
     }
 
-    public int getType() {
-        return type;
+    public int getUnitType() {
+        for(int i = 0; i < 3; i++) {
+            String part = this.type.substring(i * 3, i * 3 + 3);
+            if (!part.equals("000")) {
+                return (i + 1) * 3 - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Type: %s, Name: %s", this.type, this.name);
     }
 }
