1b681d38fdd69ab4a0e407de6c25a747c4437a64
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-03-07 10:33:13 +0300

    Update Main.java

--- src/main/java/com/iso53/Main.java
+++ src/main/java/com/iso53/Main.java
@@ -1,13 +1,35 @@
 package com.iso53;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+//        printData();
+    }
+
+    public static void printData() {
+        System.out.println("------- Incident Names Started -------");
+        System.out.println(Arrays.toString(ProblemData.INCIDENT_NAMES));
+        System.out.println("-------- Incident Names Ended --------\n");
+
+        System.out.println("------- Unit Names Started -------");
+        System.out.println(Arrays.toString(ProblemData.UNIT_NAMES));
+        System.out.println("-------- Unit Names Ended --------\n");
+
+        System.out.println("------- Incidents Started -------");
+        ProblemData.INCIDENTS.forEach(System.out::println);
+        System.out.println("-------- Incidents Ended --------\n");
+
+        System.out.println("------- Units Started -------");
+        ProblemData.UNITS.forEach(System.out::println);
+        System.out.println("-------- Units  Ended --------\n");
+
         System.out.println("------- Distance Matrix Started -------");
         Utils.printMatrix(ProblemData.DISTANCE_MATRIX);
         System.out.println("-------- Distance Matrix Ended --------\n");
 
-        System.out.println("------ District Locations Started ------");
-        ProblemData.DISTRICT_LOCATIONS.forEach(System.out::println);
-        System.out.println("------- District Locations Ended -------");
+        System.out.println("------ Process Time and Capabilities Started ------");
+        Utils.printMatrix(ProblemData.PROCESS_TIME_AND_CAPABILITIES);
+        System.out.println("------- Process Time and Capabilities Ended -------");
     }
 }
