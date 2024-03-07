04ebb86645ce7fc0011db78d2f540f102f062090
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-03-07 10:33:27 +0300

    Update Utils.java

--- src/main/java/com/iso53/Utils.java
+++ src/main/java/com/iso53/Utils.java
@@ -1,9 +1,6 @@
 package com.iso53;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -32,30 +29,114 @@ public class Utils {
         return null;
     }
 
-    public static List<District> jsonToDistricts(String filePath) {
-        List<District> districts = new ArrayList<>();
+    public static List<Incident> jsonToIncidents(String filePath) {
+        List<Incident> incidents = new ArrayList<>();
 
         try (InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(filePath)) {
             if (inputStream != null) {
                 // Read the content of the JSON file
                 InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-                JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
-                JsonArray neighborhoods = jsonObject.getAsJsonArray("neighborhoods");
+                JsonArray incidentsAsJsonArr = JsonParser.parseReader(reader).getAsJsonArray();
 
-                for (int i = 0; i < neighborhoods.size(); i++) {
-                    JsonObject neighborhood = neighborhoods.get(i).getAsJsonObject();
-                    districts.add(
-                            new District(
-                                    neighborhood.get("latitude").getAsDouble(),
-                                    neighborhood.get("longitude").getAsDouble(),
-                                    neighborhood.get("name").getAsString()));
+                for (int i = 0; i < incidentsAsJsonArr.size(); i++) {
+                    if (i == incidentsAsJsonArr.size() - 1) {
+                        JsonObject headquarter = incidentsAsJsonArr.get(i).getAsJsonObject();
+                        ProblemData.HEADQUARTER = new Headquarter(
+                                i,
+                                headquarter.get("latitude").getAsDouble(),
+                                headquarter.get("longitude").getAsDouble()
+                        );
+                        break;
+                    }
+                    JsonObject incident = incidentsAsJsonArr.get(i).getAsJsonObject();
+                    incidents.add(
+                            new Incident(
+                                    incident.get("status").getAsString(),
+                                    i,
+                                    incident.get("latitude").getAsDouble(),
+                                    incident.get("longitude").getAsDouble()
+                            )
+                    );
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return districts;
+        return incidents;
+    }
+
+    public static List<Unit> jsonToUnits(String filePath) {
+        List<Unit> units = new ArrayList<>();
+
+        try (InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(filePath)) {
+            if (inputStream != null) {
+                // Read the content of the JSON file
+                InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+                JsonArray unitsAsJsonArr = JsonParser.parseReader(reader).getAsJsonArray();
+
+                for (int i = 0; i < unitsAsJsonArr.size(); i++) {
+                    units.add(new Unit(unitsAsJsonArr.get(i).getAsJsonObject().get("type").getAsString()));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return units;
+    }
+
+    public static String[] jsonToNames(String filePath, String type) {
+        List<String> names = new ArrayList<>();
+
+        try (InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(filePath)) {
+            if (inputStream != null) {
+                // Read the content of the JSON file
+                InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+                JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+                JsonArray namesAsJsonArr = jsonObject.getAsJsonArray(type);
+
+                for (int i = 0; i < namesAsJsonArr.size(); i++) {
+                    names.add(namesAsJsonArr.get(i).getAsString());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return names.toArray(new String[0]);
+    }
+
+    public static double[][] jsonToProcessTimeAndCapabilities(String filePath) {
+        double[][] processTimeAndCapabilities = null;
+
+        try (InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(filePath)) {
+            if (inputStream != null) {
+                // Read the content of the JSON file
+                InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+                JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+                JsonArray processArray = jsonObject.getAsJsonArray("process_time_and_capabilities");
+
+                // Determine the dimensions of the array
+                int rows = processArray.size();
+                int cols = processArray.get(0).getAsJsonArray().size();
+
+                // Initialize the matrix
+                processTimeAndCapabilities = new double[rows][cols];
+
+                // Fill the matrix with values from the JSON array
+                for (int i = 0; i < rows; i++) {
+                    JsonArray rowArray = processArray.get(i).getAsJsonArray();
+                    for (int j = 0; j < cols; j++) {
+                        processTimeAndCapabilities[i][j] = rowArray.get(j).getAsDouble();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return processTimeAndCapabilities;
     }
 
     public static void printMatrix(double[][] matrix) {
