b7ad3b427d9430b435c304f779c356bee9f6de4d
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-03-07 10:33:16 +0300

    Update ProblemData.java

--- src/main/java/com/iso53/ProblemData.java
+++ src/main/java/com/iso53/ProblemData.java
@@ -3,8 +3,13 @@ package com.iso53;
 import java.util.List;
 
 public class ProblemData {
-    public static double[][] DISTANCE_MATRIX = Utils.jsonToMatrix("Bakırköy Distance Matrix.json");
-    public static List<District> DISTRICT_LOCATIONS = Utils.jsonToDistricts("Bakırköy District Locations.json");
-    public static double[][] UNITS_CAPABILITIES = Utils.jsonToMatrix("Units Capabilities.json");
-    public static double[][] PROCESS_TIME = Utils.jsonToMatrix("Process Time.json");
+    public static Headquarter HEADQUARTER;
+    public static String[] INCIDENT_NAMES = Utils.jsonToNames("Process Time and Capabilities.json", "incident_names");
+    public static String[] UNIT_NAMES = Utils.jsonToNames("Process Time and Capabilities.json", "unit_names");
+
+    public static List<Incident> INCIDENTS = Utils.jsonToIncidents("Incident Types and Locations.json");
+    public static List<Unit> UNITS = Utils.jsonToUnits("Unit Types.json");
+
+    public static double[][] DISTANCE_MATRIX = Utils.jsonToMatrix("Distance Matrix.json");
+    public static double[][] PROCESS_TIME_AND_CAPABILITIES = Utils.jsonToProcessTimeAndCapabilities("Process Time and Capabilities.json");
 }
