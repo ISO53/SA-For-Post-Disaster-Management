91a0cefbd6d87d4a1f560d78977c4320c32075a9
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-03-11 21:54:07 +0300

    added minor print adjustments

--- src/main/java/com/iso53/Main.java
+++ src/main/java/com/iso53/Main.java
@@ -1,52 +1,48 @@
 package com.iso53;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         printData();
 
-        System.out.println("\n------------- TEST BEGINS -------------\n");
-
+        // create copies so original data remains unchanged
         List<Incident> incidents = Arrays.asList(ProblemData.INCIDENTS.clone());
 
-//        Collections.shuffle(incidents);
+        //        Collections.shuffle(incidents);
 
-        // create copies so original data remains unchanged
         Scheduler.Solution solution = Scheduler.schedule(incidents.toArray(new Incident[0]), ProblemData.UNITS.clone());
-        System.out.println("\n-------------- TEST ENDS --------------\n");
 
-        System.out.println("\n------ RESULT ------");
+        System.out.println("******************************** RESULT ********************************");
         solution.printSolution();
-
     }
 
     public static void printData() {
-        System.out.println("------- Incident Names Started -------");
+        System.out.println();
+
+        System.out.println("***************************** Incident Names *****************************");
         System.out.println(Arrays.toString(ProblemData.INCIDENT_NAMES));
-        System.out.println("-------- Incident Names Ended --------\n");
+        System.out.println();
 
-        System.out.println("------- Unit Names Started -------");
+        System.out.println("******************************* Unit Names *******************************");
         System.out.println(Arrays.toString(ProblemData.UNIT_NAMES));
-        System.out.println("-------- Unit Names Ended --------\n");
+        System.out.println();
 
-        System.out.println("------- Incidents Started -------");
+        System.out.println("******************************* Incidents ********************************");
         System.out.println(Arrays.toString(ProblemData.INCIDENTS));
-        System.out.println("-------- Incidents Ended --------\n");
+        System.out.println();
 
-        System.out.println("------- Units Started -------");
+        System.out.println("********************************* Units **********************************");
         System.out.println(Arrays.toString(ProblemData.UNITS));
-        System.out.println("-------- Units  Ended --------\n");
+        System.out.println();
 
-        System.out.println("------- Distance Matrix Started -------");
+        System.out.println("**************************** Distance Matrix *****************************");
         Utils.printMatrix(ProblemData.DISTANCE_MATRIX);
-        System.out.println("-------- Distance Matrix Ended --------\n");
+        System.out.println();
 
-        System.out.println("------ Process Time and Capabilities Started ------");
+        System.out.println("******************************* Process Time and Capabilities *******************************");
         Utils.printMatrix(ProblemData.PROCESS_TIME_AND_CAPABILITIES);
-        System.out.println("------- Process Time and Capabilities Ended -------\n");
+        System.out.println();
     }
 }
