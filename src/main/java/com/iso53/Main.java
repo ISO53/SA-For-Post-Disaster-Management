e93d6d4430e18004e27acb40932298c9fde22f4d
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-03-14 00:05:16 +0300

    added grid search to find the best parameters

--- src/main/java/com/iso53/Main.java
+++ src/main/java/com/iso53/Main.java
@@ -6,23 +6,49 @@ public class Main {
     public static void main(String[] args) {
         printData();
 
+        // use grid search to find best parameters
+        GridSearch gridSearch = new GridSearch.Builder()
+                .waitCoefficient(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+                .processTimeCoefficient(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+                .distanceCoefficient(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+                .sumOfNotHandledSeverityCoefficient(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+                .unnecessaryPowerfulUnitPenaltyCoefficient(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+                .build();
+        HashMap<GridSearch.Coefficient, Double> bestParameters = gridSearch.run();
+
+        // print the best parameters
+        System.out.println("**************************** BEST PARAMETERS ***************************");
+        for (Map.Entry<GridSearch.Coefficient, Double> entry : bestParameters.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        // initialize the Scheduler object with best parameters
+        Scheduler scheduler = new Scheduler(
+                bestParameters.get(GridSearch.Coefficient.DISTANCE),
+                bestParameters.get(GridSearch.Coefficient.PROCESS_TIME),
+                bestParameters.get(GridSearch.Coefficient.SUM_OF_NOT_HANDLED_SEVERITY),
+                bestParameters.get(GridSearch.Coefficient.WAIT),
+                bestParameters.get(GridSearch.Coefficient.UNNECESSARY_POWERFUL_UNIT_PENALTY)
+        );
+
+        // iteration count
         int size = 100000;
 
-        // Solutions
+        // store the solutions here
         ArrayList<Solution> solutions = new ArrayList<>(size);
 
         // create copies so original data remains unchanged
         Incident[] incidentsArr = ProblemData.INCIDENTS.clone();
 
+        // generic algorithm-ish
         for (int i = 0; i < size; i++) {
             Mutation.scramble(incidentsArr);
-            Solution solution = Scheduler.schedule(deepCopy(incidentsArr), ProblemData.UNITS.clone());
+            Solution solution = scheduler.schedule(Utils.deepCopy(incidentsArr), ProblemData.UNITS.clone());
             solutions.add(solution);
         }
 
         // Best solution overall
         Solution minSolution = Collections.min(solutions, Comparator.comparingDouble(Solution::getScore));
-
 
         System.out.println("******************************** RESULT ********************************");
         minSolution.printSolution();
