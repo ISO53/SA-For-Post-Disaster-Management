16ed9ebe3239ccef7a6227bdbd65b40ccc4e3f7b
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-03-17 19:50:31 +0300

    mutation method are now not in-place

--- src/main/java/com/iso53/Mutation.java
+++ src/main/java/com/iso53/Mutation.java
@@ -10,32 +10,42 @@ public class Mutation<T> {
 
     private static final Random rand = new Random();
 
-    public static <T> void insert(T[] array) {
+    public static <T> T[] insert(T[] array) {
         int[] unique = rand.ints(0, array.length)
                 .distinct()
                 .limit(2)
                 .sorted() // Ensure indices are ordered
                 .toArray();
 
-        // Move the element at index2 to follow index1, shifting the rest along
-        T value = array[unique[1]];
-        System.arraycopy(array, unique[0] + 1, array, unique[0] + 2, unique[1] - unique[0] - 1);
-        array[unique[0] + 1] = value;
+        // create copy array
+        T[] newArr = Arrays.copyOf(array, array.length);
+
+        // move the element at index2 to follow index1, shifting the rest along
+        T value = newArr[unique[1]];
+        System.arraycopy(newArr, unique[0] + 1, newArr, unique[0] + 2, unique[1] - unique[0] - 1);
+        newArr[unique[0] + 1] = value;
+
+        return newArr;
     }
 
-    public static <T> void swap(T[] array) {
+    public static <T> T[] swap(T[] array) {
         int[] unique = rand.ints(0, array.length)
                 .distinct()
                 .limit(2)
                 .toArray();
 
+        // create copy array
+        T[] newArr = Arrays.copyOf(array, array.length);
+
         // Swap the elements at the two indices
-        T temp = array[unique[0]];
-        array[unique[0]] = array[unique[1]];
-        array[unique[1]] = temp;
+        T temp = newArr[unique[0]];
+        newArr[unique[0]] = newArr[unique[1]];
+        newArr[unique[1]] = temp;
+
+        return newArr;
     }
 
-    public static <T> void inversion(T[] array) {
+    public static <T> T[] inversion(T[] array) {
         int[] unique = rand.ints(0, array.length)
                 .distinct()
                 .limit(2)
@@ -48,15 +58,20 @@ public class Mutation<T> {
             unique[1] = temp;
         }
 
+        // create copy array
+        T[] newArr = Arrays.copyOf(array, array.length);
+
         // Invert the substring between the two indices
         for (int i = unique[0], j = unique[1]; i < j; i++, j--) {
-            T temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            T temp = newArr[i];
+            newArr[i] = newArr[j];
+            newArr[j] = temp;
         }
+
+        return newArr;
     }
 
-    public static <T> void scramble(T[] array) {
+    public static <T> T[] scramble(T[] array) {
         // Generate a random subset size between 1 and array length
         int subsetSize = rand.nextInt(array.length) + 1;
 
@@ -74,9 +89,14 @@ public class Mutation<T> {
         // Randomly rearrange the elements
         Collections.shuffle(subset, rand);
 
-        // Put the rearranged elements back into the array
+        // create copy array
+        T[] newArr = Arrays.copyOf(array, array.length);
+
+        // Put the rearranged elements back into the new array
         for (int i = 0; i < indices.length; i++) {
-            array[indices[i]] = subset.get(i);
+            newArr[indices[i]] = subset.get(i);
         }
+
+        return newArr;
     }
 }
